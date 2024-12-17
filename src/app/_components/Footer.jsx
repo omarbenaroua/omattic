ab6053ebb1fc715345b38bb3b9ed22f2978bf4da
omarbenaroua <benarouaomar@gmail.com>
2024-12-17 05:54:10 +0100

    first commit

--- src/app/_components/Footer.jsx
+++ src/app/_components/Footer.jsx
@@ -78,29 +78,7 @@ const Footer = () => {
           </div>
 
           <div className="mt-12 border-t border-gray-100 pt-6">
-            <div className="text-center sm:flex sm:justify-between sm:text-left">
-              <p className="text-sm text-gray-500">
-                <span className="block sm:inline">
-                  Tutti i diritti riservati.
-                </span>
-
-                <a
-                  className="inline-block text-teal-600 underline transition hover:text-teal-600/75"
-                  href="#"
-                >
-                  Termine & Condizioni
-                </a>
-
-                <span>&middot;</span>
-
-                <a
-                  className="inline-block text-teal-600 underline transition hover:text-teal-600/75"
-                  href="#"
-                >
-                  Privacy Policy
-                </a>
-              </p>
-
+            <div className="text-center sm:flex justify-center">
               <p className="mt-4 text-sm text-gray-500 sm:order-first sm:mt-0">
                 &copy; 2024 Omar ben aroua
               </p>
